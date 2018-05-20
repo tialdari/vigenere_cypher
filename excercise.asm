fd92d8093284bbf57516a60c87da63f1f5221c9e
Asia <asialuk98@gmail.com>
2018-05-20 21:04:14 +0200

    Clean the encryption method

--- excercise.asm
+++ excercise.asm
@@ -1,5 +1,5 @@
 .data
-    text: .asciiz "dozwoloneznakiheh"
+    text: .asciiz "dozwoloneznaki"
     key: .asciiz "klucz"
     line: .asciiz "\n"
     space: .asciiz "  "
@@ -18,41 +18,23 @@
    		bgt $t0, 4, resetCounter
    		bgt $t1, 16, exit
    		
-   		li $v0, 11
    		lb $a0, key($t0)
-   		syscall
-   		
    		move $t2, $a0
    		
-   		li $v0, 4
-   		la $a0, space
-   		syscall
-   		
-   		li $v0, 11
    		lb $a0, text($t1)
-   		syscall
+ 		move $t3, $a0
    		
-   		move $t3, $a0
-   		
-   		li $v0, 4
-   		la $a0, arrow
-   		syscall
-   		
-   		add $t2, $t2, $t3
+		add $t2, $t2, $t3
    		sub $t2, $t2, 194
    		
    		bgt $t2, 25, substract
    		
-
    	continue:
-   		 add $t2, $t2, 97
+   	
+   		add $t2, $t2, 97
    		
    		li $v0, 11
    		move $a0, $t2
-   		syscall
-   		
-   		li $v0, 4
-   		la $a0, line
    		syscall
    		
    		addi $t0, $t0, 1

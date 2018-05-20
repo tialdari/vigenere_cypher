36f7d9f91bfd2c47a792d6a29ea8c70e429a5a88
Asia <asialuk98@gmail.com>
2018-05-20 13:40:18 +0200

    Iterate over a string

--- char_printing.asm
+++ char_printing.asm
@@ -1,19 +1,35 @@
 .data
-    string: .asciiz "stringer"
+    string: .asciiz "domek "
     char: .asciiz "c"
 
 .text
 .globl main
     main:   
 
+   	la $a0, string 
+   	addi $t0, $zero, 0  
+   			           # load the addr of the given string into $a0. 
+	
+	
+	while:
+	#sbeqz $a1, exit
 
-   la $a0, string              # load the addr of the given string into $a0. 
-   
 
-    li $v0, 11                  # service 11 is print character
-    lb  $a0, 0 ($a0)
-    syscall
+	
+   	 li $v0, 11                  # service 11 is print character
+   	 lb  $a0, string($t0)
+   	    	 beqz $a0, exit
+   	 syscall
+   	 
 
+   	 
+   	 addi, $t0, $t0, 1
+   	 
+   	j while
+  
     exit:
     li $v0, 10                # return control to SPIM OS
     syscall
+   	 
+   	 
+   
